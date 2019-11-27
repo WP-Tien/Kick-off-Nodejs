23e69b50ad74b776bf0c918e955ad3b220609788
nguyenhuutien3895 <nguyenhuutien.it.3895@gmail.com>
2019-11-27 22:30:52 +0700

    Mot vai ham arr

---
+++ 3-Mang.js
@@ -0,0 +1,10 @@
+var mang = ["Cam", "Xoai", "Chuoi"];
+
+console.log( mang[2] ); // 0
+
+// Dem mang bao phan tu
+console.log( mang.length ); // 1
+
+mang.push("CoCo");
+
+console.log( mang.length );
