25452cd33b6ab10c883a2f9a716a50c244148cc6
nguyenhuutien3895 <nguyenhuutien.it.3895@gmail.com>
2019-11-28 12:44:15 +0700

    params

--- Server/app.js
+++ Server/app.js
@@ -12,3 +12,10 @@ app.get("/", function(req, res) {
 app.get("/gioithieu", function(req, res) {
     res.send('I\'m a ROUTE');
 })
+
+app.get("/tinhtong/:so1/:so2", function(req, res) { // tham so bat dau dau :
+    var so1 = req.params.so1 // req la` thong tin khach hang` gui len
+    var so2 = req.params.so2 
+    var sum =  parseInt( so1 ) + parseInt ( so2 );
+    res.send( " <h1> Result </h1> " + sum );
+})
